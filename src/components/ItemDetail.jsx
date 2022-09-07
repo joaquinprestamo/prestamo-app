a7018fc4e8ed8c96bee00a27fb5fef183a6c3d28
Joaquín Préstamo <jprestamo@artech-consulting.com>
2022-09-07 15:26:01 -0300

    Corrección Entrega

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,7 +4,7 @@ import AddCart from "./AddCart";
 
 const ItemDetail = ({info})=>{
     return (
-        <div className="col-12 my-3">
+        <div className="col-12 my-3">            
             <div className="row">
                 <div className="col-md-6">
                     <img src={"./assets/images/shop/" + info.image} alt="..." className="detail-pic mb-3" />
@@ -14,11 +14,11 @@ const ItemDetail = ({info})=>{
                     <p className="mb-1">{info.description}</p>
                     <p className="mb-3 fs-9">Últimos {info.stock} Disponibles</p>
                     <ul className="mb-3 fs-7 product-extra">
-                    {info.leches.map((leche, i) => {
-                        return(
-                            <li key={i}><button>{leche}</button></li>
-                        );
-                    })}
+                        {info.leches.map((leche, i) => {
+                            return(
+                                <li key={i}><button>{leche}</button></li>
+                            );
+                        })}
                     </ul>
                     <div className="shop-price">$ {info.price}</div>
                     <ItemCount stock={info.stock} />
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -3,21 +3,25 @@ import ItemDetail from "./ItemDetail";
 
 
 const product = {id:1, name:"Latte & Medialuna", description:"Latte + Medialuna + Jugo de Naranja", image:"promo-latte.jpg", price:650, stock:5, leches:["vacuna", "almendra", "soja"] };
+let loading = true;
 
 export const ItemDetailContainer = ()=>{
-    const [item,setItem] = useState([]);
+    const [item,setItem] = useState([]);    
     useEffect(()=>{
         const getItem = new Promise(resolve => {
             setTimeout(()=>{
                 resolve(product);
             }, 2000);
         });
-        getItem.then(res => setItem(res));
+        getItem.then(res => {
+            setItem(res);
+            loading = false;
+        });
     }, []);
 
     return(
-        <div className="container my-5">    
-            <ItemDetail info = {item} />
+        <div className="container my-5">   
+            {loading ? <p>cargando</p> : <ItemDetail info = {item} />}             
         </div>
     );
 };
